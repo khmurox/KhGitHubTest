30ed484721f4f9f7cfc21fa19c724b3e9b02d0e7
khmurox <kh.murox@gmail.com>
2014-07-08 10:27:33 +0200

    Caffeine as own project

---
+++ Caffeine/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Caffeine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        //Caffeine is a useful program to keep your monitors and/or system awake using power management APIs and shows 
+        //how to properly adjust and restore window positions to support multiple monitors.
+        //http://code.msdn.microsoft.com/Caffeine-c9988981
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
