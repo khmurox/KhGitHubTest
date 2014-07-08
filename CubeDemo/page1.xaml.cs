93486c53c0c33e7a46307e0e7b884237b3695255
khmurox <kh.murox@gmail.com>
2014-07-08 11:38:14 +0200

    Add files to cubePatch

--- CubeDemo/page1.xaml.cs
+++ CubeDemo/page1.xaml.cs
@@ -1,23 +1,10 @@
 using System;
-using System.Data;
 using System.Windows;
-using System.Windows.Data;
-using System.Configuration;
-using System.Windows.Media;
-using System.Windows.Media.Media3D;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Reflection;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Threading;
-using System.IO;
-using DemoDev;
 
-namespace Ribbon
+namespace CubeDemo
 {
-
-    public partial class Page1 : Page
+    public partial class Page1
     {
         public Page1()
         {
@@ -27,31 +14,23 @@ namespace Ribbon
         private void OnLoaded(object sender, EventArgs e)
         {
             // setup trackball for moving the model around
-            _trackball = new Trackball();
+            _trackball = new Trackball(true);
             _trackball.Attach(this);
             _trackball.Slaves.Add(MyViewport3D);
             _trackball.Enabled = true;
-
         }
 
         #region Events
         private void OnImage1Animate(object sender, RoutedEventArgs e)
         {
-            Storyboard s;
-
-            s = (Storyboard)this.FindResource("RotateStoryboard");
-            this.BeginStoryboard(s);
+            var s = (Storyboard)FindResource("RotateStoryboard");
+            BeginStoryboard(s);
         }
-        #endregion
 
-        #region Globals
+        #endregion
 
         Trackball _trackball;
-
-        #endregion
     }
-
-   
 }
 
 
--- CubeDemo/trackball.cs
+++ CubeDemo/trackball.cs
@@ -4,53 +4,43 @@
 // </copyright>
 //---------------------------------------------------------------------------
 
-
-using System;
 using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Controls;
-using System.Windows.Media;
+using System.Windows.Input;
 using System.Windows.Media.Media3D;
 
-
-namespace DemoDev
+namespace CubeDemo
 {
     public class Trackball
     {
-        public Trackball()
+        public Trackball(bool translating)
         {
-            Vector3D _translate = new Vector3D(0, 0, 0);
-            Vector3D _translateDelta = new Vector3D(0, 0, 0);
-
+            _translating = translating;
+            var tr = new Vector3D(0, 0, 0);
+            var trDe = new Vector3D(0, 0, 0);
             Reset();
         }
 
         public void Attach(FrameworkElement element)
         {
-            element.MouseMove += this.MouseMoveHandler;
-            element.MouseRightButtonDown += this.MouseDownHandler;
-            element.MouseRightButtonUp += this.MouseUpHandler;
-            element.MouseWheel += this.OnMouseWheel;
+            element.MouseMove += MouseMoveHandler;
+            element.MouseRightButtonDown += MouseDownHandler;
+            element.MouseRightButtonUp += MouseUpHandler;
+            element.MouseWheel += OnMouseWheel;
         }
 
         public void Detach(FrameworkElement element)
         {
-            element.MouseMove -= this.MouseMoveHandler;
-            element.MouseRightButtonDown -= this.MouseDownHandler;
-            element.MouseRightButtonUp -= this.MouseUpHandler;
-            element.MouseWheel -= this.OnMouseWheel;
+            element.MouseMove -= MouseMoveHandler;
+            element.MouseRightButtonDown -= MouseDownHandler;
+            element.MouseRightButtonUp -= MouseUpHandler;
+            element.MouseWheel -= OnMouseWheel;
         }
 
         public List<Viewport3D> Slaves
         {
-            get
-            {
-                if (_slaves == null)
-                    _slaves = new List<Viewport3D>();
-
-                return _slaves;
-            }
+            get { return _slaves ?? (_slaves = new List<Viewport3D>()); }
             set
             {
                 _slaves = value;
@@ -72,52 +62,60 @@ namespace DemoDev
         // Updates the matrices of the slaves using the rotation quaternion.
         private void UpdateSlaves(Quaternion q, double s, Vector3D t)
         {
-
             if (_slaves != null)
             {
                 foreach (Viewport3D i in _slaves)
                 {
-                    ModelVisual3D mv = i.Children[0] as ModelVisual3D;
-                    Transform3DGroup t3dg = mv.Transform as Transform3DGroup;
+                    var mv = i.Children[0] as ModelVisual3D;
+                    if (mv != null)
+                    {
+                        var t3Dg = mv.Transform as Transform3DGroup;
 
-                    ScaleTransform3D _GroupScaleTransform = t3dg.Children[0] as ScaleTransform3D;
-                    RotateTransform3D _GroupRotateTransform = t3dg.Children[1] as RotateTransform3D;
-                    TranslateTransform3D _GroupTranslateTransform = t3dg.Children[2] as TranslateTransform3D;
+                        var groupScaleTransform = t3Dg.Children[0] as ScaleTransform3D;
+                        var groupRotateTransform = t3Dg.Children[1] as RotateTransform3D;
+                        var groupTranslateTransform = t3Dg.Children[2] as TranslateTransform3D;
 
-                    _GroupScaleTransform.ScaleX = s;
-                    _GroupScaleTransform.ScaleY = s;
-                    _GroupScaleTransform.ScaleZ = s;
-                    _GroupRotateTransform.Rotation = new AxisAngleRotation3D(q.Axis, q.Angle);
-                    _GroupTranslateTransform.OffsetX = t.X;
-                    _GroupTranslateTransform.OffsetY = t.Y;
-                    _GroupTranslateTransform.OffsetZ = t.Z;
-
+                        if (groupScaleTransform != null)
+                        {
+                            groupScaleTransform.ScaleX = s;
+                            groupScaleTransform.ScaleY = s;
+                            groupScaleTransform.ScaleZ = s;
+                        }
+                        if (groupRotateTransform != null)
+                            groupRotateTransform.Rotation = new AxisAngleRotation3D(q.Axis, q.Angle);
+                        if (groupTranslateTransform != null)
+                        {
+                            groupTranslateTransform.OffsetX = t.X;
+                            groupTranslateTransform.OffsetY = t.Y;
+                            groupTranslateTransform.OffsetZ = t.Z;
+                        }
+                    }
                 }
             }
         }
 
 
-        private void MouseMoveHandler(object sender, System.Windows.Input.MouseEventArgs e)
+        private void MouseMoveHandler(object sender, MouseEventArgs e)
         {
             if (!Enabled) return;
             e.Handled = true;
 
-            UIElement el = (UIElement)sender;
+            var el = (UIElement)sender;
 
             if (el.IsMouseCaptured)
             {
                 Vector delta = _point - e.MouseDevice.GetPosition(el);
-                Vector3D t = new Vector3D();
+                var t = new Vector3D();
 
                 delta /= 2;
                 Quaternion q = _rotation;
 
-                if (_rotating == true)
+                if (_rotating)
                 {
                     // We can redefine this 2D mouse delta as a 3D mouse delta
                     // where "into the screen" is Z
-                    Vector3D mouse = new Vector3D(delta.X, -delta.Y, 0);
-                    Vector3D axis = Vector3D.CrossProduct(mouse, new Vector3D(0, 0, 1));
+                    var mouse = new Vector3D(delta.X, -delta.Y, 0);
+                    var axis = Vector3D.CrossProduct(mouse, new Vector3D(0, 0, 1));
                     double len = axis.Length;
                     if (len < 0.00001 || _scaling)
                         _rotationDelta = new Quaternion(new Vector3D(0, 0, 1), 0);
@@ -145,28 +143,25 @@ namespace DemoDev
             e.Handled = true;
 
 
-            if (Keyboard.IsKeyDown(Key.F1) == true)
+            if (Keyboard.IsKeyDown(Key.F1))
             {
                 Reset();
                 return;
             }
 
-            UIElement el = (UIElement)sender;
+            var el = (UIElement)sender;
             _point = e.MouseDevice.GetPosition(el);
             // Initialize the center of rotation to the lookatpoint
             if (!_centered)
             {
-                ProjectionCamera camera = (ProjectionCamera)_slaves[0].Camera;
+                var camera = (ProjectionCamera)_slaves[0].Camera;
                 _center = camera.LookDirection;
                 _centered = true;
             }
 
             _scaling = (e.MiddleButton == MouseButtonState.Pressed);
 
-            if (Keyboard.IsKeyDown(Key.Space) == false)
-                _rotating = true;
-            else
-                _rotating = false;
+            _rotating = Keyboard.IsKeyDown(Key.Space) == false;
 
             el.CaptureMouse();
         }
@@ -178,7 +173,7 @@ namespace DemoDev
 
             // Stuff the current initial + delta into initial so when we next move we
             // start at the right place.
-            if (_rotating == true)
+            if (_rotating)
                 _rotation = _rotationDelta * _rotation;
             else
             {
@@ -188,14 +183,14 @@ namespace DemoDev
             }
 
             //_scale = _scaleDelta*_scale;
-            UIElement el = (UIElement)sender;
+            var el = (UIElement)sender;
             el.ReleaseMouseCapture();
         }
-        void OnMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+        void OnMouseWheel(object sender, MouseWheelEventArgs e)
         {
             e.Handled = true;
 
-            _scaleDelta += (double)((double)e.Delta / (double)1000);
+            _scaleDelta += e.Delta / (double)1000;
             Quaternion q = _rotation;
 
             UpdateSlaves(q, _scale * _scaleDelta, _translate);
@@ -208,7 +203,7 @@ namespace DemoDev
 
         private void Reset()
         {
-            _rotation = new Quaternion (0,0,0,1);
+            _rotation = new Quaternion(0, 0, 0, 1);
             _scale = 1;
             _translate.X = 0;
             _translate.Y = 0;
@@ -227,17 +222,17 @@ namespace DemoDev
         // The state of the trackball
         private bool _enabled;
         private Vector3D _center;
-        private bool _centered;   
+        private bool _centered;
         private double _scale;
         private Vector3D _translate;
         private Quaternion _rotation;
         private List<Viewport3D> _slaves;
 
         // The state of the current drag
-        private bool _scaling;              
+        private bool _scaling;
         private double _scaleDelta;          // Change to scale because of this drag
         private Quaternion _rotationDelta;  // Change to rotation because of this drag
-        private System.Windows.Point _point;               // Initial point of drag
+        private Point _point;               // Initial point of drag
         private Vector3D _translateDelta;
         private bool _rotating;
         private bool _translating;
