7c59050a9bacf632a31ab4fc3d762cc47d6162eb
khmurox <kh.murox@gmail.com>
2014-07-21 08:44:33 +0200

    Optimize references. Fixes #1

--- CubeDemo/page1.xaml.cs
+++ CubeDemo/page1.xaml.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Data;
 using System.Windows;
 using System.Windows.Data;
 using System.Configuration;
--- PuzzleDemo/App.xaml.cs
+++ PuzzleDemo/App.xaml.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Windows;
-using System.Data;
-using System.Xml;
 using System.Configuration;
 
 namespace PuzzleProject
